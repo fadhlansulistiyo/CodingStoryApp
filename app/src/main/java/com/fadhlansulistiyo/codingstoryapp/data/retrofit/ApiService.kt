99e5570d6afac41c8a2245ab983f9db797f2ddc8
Fadhlan Sulistiyo Hidayat <fadhlansulistiyo@gmail.com>
2024-07-11 20:01:47 +0700

    Change java version to 17 and add RequiresOptIn

--- app/src/main/java/com/fadhlansulistiyo/codingstoryapp/data/retrofit/ApiService.kt
+++ app/src/main/java/com/fadhlansulistiyo/codingstoryapp/data/retrofit/ApiService.kt
@@ -14,6 +14,7 @@ import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiService {
     @FormUrlEncoded
@@ -32,7 +33,10 @@ interface ApiService {
     ): LoginResponse
 
     @GET("stories")
-    suspend fun getStories(): StoryResponse
+    suspend fun getStories(
+        @Query("page") page: Int = 1,
+        @Query("size") size: Int = 20
+    ): StoryResponse
 
     @GET("stories/{id}")
     suspend fun getDetailStories(
